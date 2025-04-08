87f8eda8c87e9e1a0199c4f1d8901505b4edafcb
Rabimishra1 <122787363+Rabimishra1@users.noreply.github.com>
2025-04-08 23:22:08 +0530

    Deploy to GitHub Pages

---
+++ client/precache-manifest.886c44356f730e55d3e9b1b3d88f9203.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7274688bceeea9422597043a0b7ff0ce",
+    "url": "/waterwise/index.html"
+  },
+  {
+    "revision": "fc9381c8b486d9739e39",
+    "url": "/waterwise/static/css/main.bf93b3ff.chunk.css"
+  },
+  {
+    "revision": "66080558a16636123dbe",
+    "url": "/waterwise/static/js/2.586481e2.chunk.js"
+  },
+  {
+    "revision": "475a116d3c88af66225e7c03a493ad5f",
+    "url": "/waterwise/static/js/2.586481e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd8cf0af2a5f94afa48",
+    "url": "/waterwise/static/js/3.10f8bd52.chunk.js"
+  },
+  {
+    "revision": "fc9381c8b486d9739e39",
+    "url": "/waterwise/static/js/main.2373f60f.chunk.js"
+  },
+  {
+    "revision": "ff55dca523694aa04ddd",
+    "url": "/waterwise/static/js/runtime-main.38757b76.js"
+  },
+  {
+    "revision": "7288dc7d5a576175e9beea74d79ccd99",
+    "url": "/waterwise/static/media/BG.7288dc7d.mp4"
+  },
+  {
+    "revision": "3a1b6cd4e042963e9494eaebe752e7c3",
+    "url": "/waterwise/static/media/logo.3a1b6cd4.png"
+  },
+  {
+    "revision": "8725d0790e59a6bc4ef8d63666d85298",
+    "url": "/waterwise/static/media/water_conservation.8725d079.mp4"
+  }
+]);
